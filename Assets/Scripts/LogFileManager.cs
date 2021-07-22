c597537b3dcc0626a84366dbcc3255d22f7f133f
BenBinary <43401683+BenBinary@users.noreply.github.com>
2021-07-22 20:32:28 +0200

    event manager and log manager added

---
+++ Assets/Scripts/LogFileManager.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR;
+
+[Serializable]
+public class LogFileManager : MonoBehaviour
+{
+    public static string DataPath;
+    private static System.Random _rand;
+    private static int _fileId;
+    private static string _concatenatedLogString;
+    private static DateTimeOffset _dto;
+
+    public Text LogText;
+
+    private void Start()
+    {
+        _rand = new System.Random();
+        _fileId = _rand.Next(1, 999999);
+        DataPath = Application.persistentDataPath;
+    }
+
+    private void Update()
+    {
+        LogText.text = _concatenatedLogString;
+    }
+
+    public void WriteFile()
+    {
+        DataPath += "/UGIW_Logfile_" + _fileId + ".ugiw";
+        File.WriteAllText(DataPath, _concatenatedLogString);
+    }
+
+    public void TriggerPressed()
+    {
+        _dto = DateTimeOffset.Now;
+        _concatenatedLogString += "| UGIW | Trigger-Pressed | "
+            + _dto.LocalDateTime.ToString("dd/MM/yyyy hh:mm:ss.fff tt") + " |" + "\n";
+    }
+
+    public void GeneralPurposeLogMessage(string message)
+    {
+        _dto = DateTimeOffset.Now;
+        _concatenatedLogString += "| UGIW | General_msg | " + message + " |"
+            + _dto.LocalDateTime.ToString("dd/MM/yyyy hh:mm:ss.fff tt") + " |" + "\n";
+    }
+    public void PickedUp(GameObject controller)
+    {
+        _dto = DateTimeOffset.Now;
+        _concatenatedLogString += "| UGIW | Object picked up with " + controller.name + " | "
+            + _dto.LocalDateTime.ToString("dd/MM/yyyy hh:mm:ss.fff tt") + " |" + "\n";
+    }
+
+    public void Released(GameObject controller)
+    {
+        _dto = DateTimeOffset.Now;
+        _concatenatedLogString += "| UGIW | Object dropped with " + controller.name + " | "
+            + _dto.LocalDateTime.ToString("dd/MM/yyyy hh:mm:ss.fff tt") + " |" + "\n";
+    }
+
+    public void EndApp()
+    {
+        GeneralPurposeLogMessage("Saved on device!");
+        WriteFile();
+    }
+}
